ab07a478635a3a28ddfcdb72891439240efd6ec5
mi_ce <mi_ce@uni-bremen.de>
2020-12-14 22:00:41 +0100

    text adjust

--- Game/example/sketch.js
+++ Game/example/sketch.js
@@ -193,5 +193,5 @@ function setPos(position) {
   background(0);
   fill(255);
   textSize(32);
-  text("Current position: " + nf(lat,2,2) + " " + nf(lng,2,2), 10, height/2);
+  text("Current position: " + nf(lat,2,2) + " " + nf(lng,2,2), 10, height/8);
 }
