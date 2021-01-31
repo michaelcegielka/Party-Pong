7665e0a66f23651b8f2d8eb915e1501d5e67ee0d
mi_ce <mi_ce@uni-bremen.de>
2021-01-31 20:01:42 +0100

    ...

---
+++ partyPong/code/menus/empty.js
@@ -0,0 +1,12 @@
+function Emptyvalidation(inputtxt)
+      {
+ if (inputtxt.value.length == 0)
+      {
+ return "try again";
+      }
+ else
+      {
+        document.location.href = "menu.html";
+    }
+
+}
